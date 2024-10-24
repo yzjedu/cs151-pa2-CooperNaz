531039e2393cd3da155211f939105aaae8244940
CooperNazar <ccnazar@loyola.edu>
2024-10-23 21:38:42 -0400

    More HCI confusion

--- main.py
+++ main.py
@@ -10,5 +10,8 @@
 # Output purpose of the program
 print('Hello! Welcome to the Game of Sticks!')
 print('This game includes three players, one of which is part of the program!')
-print('Rules:'
-      )
+print('RULES:'
+      '1. The game starts with a set amount of sticks: 10 or 100'
+      '2. Three players take turns choosing how many sticks to take'
+      '3. On your turn, you must take either 1, 2, or 3 sticks'
+      '4. The player who takes the last stick loses')
