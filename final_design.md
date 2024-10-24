adcb1dbcbf875a59f0fdc8e69609073a0d52de36
CooperNazar <ccnazar@loyola.edu>
2024-10-23 22:41:57 -0400

    Code done, reflection and flowchart coming soon

--- final_design.md
+++ final_design.md
@@ -1,2 +1,36 @@
 # Final Design Document
 #### PLEASE! PLEASE! PLEASE! Complete all the NOs in the feedback document 
+
+1. Output the purpose of the program and the rules of the game
+2. Set the sentinel to 'no'
+3. Set losses to 0
+4. While the user does not say 'no' to playing again:
+   1. Prompt the user to choose between starting the game with 10 and 100 sticks and set the number of total sticks accordingly
+   2. While the amount of total sticks is greater than zero:
+       1. If the amount of sticks is greater than 0:
+          1. Prompt player 1 to input the number of sticks they want to take
+          2. While player 1's choice is not 1, 2, or 3:
+             1. Output that the value is invalid
+             2. Prompt the user to try again
+          3. Subtract the number of sticks taken from the number of total sticks
+          4. Output the number of sticks player 1 chose
+          5. Output the number of sticks left
+      2. If the amount of sticks is greater than 0:
+          1. Prompt player 2 to input the number of sticks they want to take
+          2. While player 2's choice is not 1, 2, or 3:
+             1. Output that the value is invalid
+             2. Prompt the user to try again
+          3. Subtract the number of sticks taken from the number of total sticks
+          4. Output how many sticks player 2 chose
+          5. Output the number of sticks left
+       3. If the amount of sticks is greater than 0:
+          1. For player 3, the computer, generate a random number from 1 to 3 and assign that to the choice
+          2. Subtract the number of sticks taken from the number of total sticks
+          3. Output how many sticks player 3 chose
+          4. Output the number of sticks left
+   3. Assign the loss to whoever takes the last stick
+   4. Output whether player 1, 2, or 3 lost
+   5. Output the number of losses for each player
+   6. Prompt the user to input whether they want to play again or not
+5. Output the number of losses for each player
+6. Output 'thank you for playing'
--- main.py
+++ main.py
@@ -7,11 +7,118 @@
 # Data Out: Losses
 # Credits:
 
+import random
+
 # Output purpose of the program
-print('Hello! Welcome to the Game of Sticks!')
+print('\nHello! Welcome to the Game of Sticks!')
 print('This game includes three players, one of which is part of the program!')
-print('RULES:'
-      '1. The game starts with a set amount of sticks: 10 or 100'
-      '2. Three players take turns choosing how many sticks to take'
-      '3. On your turn, you must take either 1, 2, or 3 sticks'
-      '4. The player who takes the last stick loses')
+
+# Output the rules
+print('\nRULES:')
+print('\t1. The game starts with a set amount of sticks: 10 or 100')
+print('\t2. Three players take turns choosing how many sticks to take')
+print('\t3. On your turn, you must take either 1, 2, or 3 sticks')
+print('\t4. The player who takes the last stick loses')
+
+# Set the sentinel to 'no'
+SENTINEL = 'no'
+
+# Create a variable for the play again decision
+replay = 'yes'
+
+# Set all players' losses to 0
+player1_loss = 0
+player2_loss = 0
+player3_loss = 0
+
+# Start the loop that will run until the player decides to stop
+while replay != 'no' and replay == 'yes':
+
+      # Ask the user to choose which game mode they will play
+      print('\nGAME MODES:')
+      print('\t1. 10 sticks')
+      print('\t2. 100 sticks')
+      game_mode = int(input('\nWhich game mode would you like to play? '))
+
+      # Make sure that the user chooses game mode 1 or 2
+      while game_mode != 1 and game_mode != 2:
+            print('\nInvalid input. Please try again.')
+            game_mode = int(input('\nWhich game mode would you like to play? '))
+
+      # Set the number of total sticks based on the game mode
+      if game_mode == 1:
+            total_sticks = 10
+      else:
+            total_sticks = 100
+      sticks = total_sticks
+
+      # Set up each player's turn to run when the number of total sticks is greater than zero
+      # For each, subtract the player's choice from the number of sticks and output the remaining sticks
+      # When a player is left with one stick, they lose because they can only take the last stick
+      while sticks > 0:
+            if sticks > 1:
+                  player1_choice = int(input('\nPlayer 1, how many sticks would you like to take? '))
+                  while player1_choice != 1 and player1_choice != 2 and player1_choice != 3:
+                        print('\nInvalid input. Please try again.')
+                        player1_choice = int(input('\nPlayer 1, how many sticks would you like to take? '))
+                  sticks -= player1_choice
+                  print(f'\nThere are {sticks} sticks left.')
+            elif sticks == 1 or sticks == 0:
+                  print('\nPlayer 1 loses! Thanks for playing!')
+                  sticks = -10 # Random value that can't be achieved through user input
+                  player1_loss += 1
+            else:
+                  print('')
+
+            if sticks > 1:
+                  player2_choice = int(input('\nPlayer 2, how many sticks would you like to take? '))
+                  while player2_choice != 1 and player2_choice != 2 and player2_choice != 3:
+                        print('\nInvalid input. Please try again.')
+                        player2_choice = int(input('\nPlayer 2, how many sticks would you like to take? '))
+                  sticks -= player2_choice
+                  print(f'\nThere are {sticks} sticks left.')
+            elif sticks == 1 or sticks == 0:
+                  print('\nPlayer 2 loses! Thanks for playing!')
+                  sticks = -10
+                  player2_loss += 1
+            else:
+                  print('')
+
+            if sticks > 1:
+                  player3_choice = random.randint(1,3)
+                  sticks -= player3_choice
+                  print(f'\nPlayer 3 chose {player3_choice} sticks.')
+                  print(f'\nThere are {sticks} sticks left.')
+            elif sticks == 1 or sticks == 0:
+                  print('\nPlayer 3 loses! Thanks for playing!')
+                  sticks = -10
+                  player3_loss += 1
+            else:
+                  print('')
+
+
+
+      # Output the number of losses for each player
+
+      print('\nRESULTS:')
+      print(f'\n\tPlayer 1 lost {player1_loss} times.')
+      print(f'\n\tPlayer 2 lost {player2_loss} times.')
+      print(f'\n\tPlayer 3 lost {player3_loss} times.')
+
+      # Prompt the user to input if they want to play again or not
+      replay = input('\nWould you like to play again? ')
+      replay = replay.lower().strip()
+      if replay != 'yes' and replay != 'no':
+            print('\nInvalid input. Please input yes or no.')
+            replay = input('\nWould you like to play again? ')
+            replay = replay.lower().strip()
+
+# Output the number of losses for each player
+print('\nRESULTS:')
+print(f'\n\tPlayer 1 lost {player1_loss} times.')
+print(f'\n\tPlayer 2 lost {player2_loss} times.')
+print(f'\n\tPlayer 3 lost {player3_loss} times.')
+
+# Print a thank-you message
+print('\nThank you for playing!')
+print('\nClosing the game.\n')
